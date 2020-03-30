87a576f9ad033f4799f76aeb9efeebce421fb2af
lilly <ellis.l@rangiruru.school.nz>
2020-03-30 13:25:16 +1300

    Added inputs for name and temperature.

--- src/index.svelte
+++ src/index.svelte
@@ -1,3 +1,19 @@
-<h1>Svelte</h1>
+<script>
+let name = ""
+let temp = 20
+ </script>
 
-<p>Welcome to coding with Svelte!</p>
+<h1>Hot Chocolate </h1>
+
+<label>
+Name:
+<input type="text" bind:value={name}>
+</label>
+
+
+<label>
+Drink temperature:
+ <input type="range" min="0" max="100" bind:value={temp}>
+</label>
+
+<p>Hi {name}, your drink is {temp} degrees.</p>
